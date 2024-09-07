8e6dea0ec7902c0189563b69852506e2e5983838
KaterynkaPet <katerinapetrenko85@gmail.com>
2024-09-08 00:10:12 +0300

    add patch and delete

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,7 +6,7 @@ export const getAllContactsController = async (req, res) => {
     const data = await contactServices.getAllContacts();
         
     res.json({
-        tatus: 200,
+        status: 200,
         message: "Successfully found contacts!",
         data,
     });
@@ -28,4 +28,54 @@ export const getAllContactByIdController = async (req, res) => {
         data,
     });
  
+};
+
+export const addContactController = async (req, res) => {
+    const data = await contactServices.createContact(req.body);
+  
+    
+    res.status(201).json({
+        status: 201,
+        message: "Successfully created a contact!",
+        data,
+    });
+};
+
+export const upsertContactController = async(req, res) => {
+    const {id} = req.params;
+    const {isNew, data} = await contactServices.updateContact({ _id: id }, req.body, { upsert: true });
+    
+    const status = isNew ? 201 : 200;
+
+   res.status(status).json({
+        status,
+        message: "Successfully upsert a contact!",
+        data,
+    });
+};
+
+export const patchContactController = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactServices.updateContact({ _id: id }, req.body);
+    
+    if (!result) {
+        throw createHttpError(404, `Contact width id=${id} not found`);
+    }
+    
+    res.json({
+        status: 200,
+        message: "Successfully patched a contact!",
+        data: result.data,
+    });
+};
+
+export const deleteContactController = async (req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.deleteContact({ _id: id });
+
+    if (!data) {
+        throw createHttpError(404, `Contact width id=${id} not found`);
+    }
+
+    res.status(204).send();
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import { getAllContactsController, getAllContactByIdController } from "../controllers/contacts.js";
+import * as contactConrollers from "../controllers/contacts.js";
 
 import ctrlWrapper from "../utils/ctrlWrapper.js";
 
 const contactsRouter = Router();
 
-contactsRouter.get("/",ctrlWrapper(getAllContactsController));
+contactsRouter.get("/",ctrlWrapper(contactConrollers.getAllContactsController));
 
-    contactsRouter.get("/:id", ctrlWrapper(getAllContactByIdController));
+contactsRouter.get("/:id", ctrlWrapper(contactConrollers.getAllContactByIdController));
+
+contactsRouter.post("/", ctrlWrapper(contactConrollers.addContactController));
+
+contactsRouter.put("/:id", ctrlWrapper(contactConrollers.upsertContactController));
+
+contactsRouter.patch("/:id", ctrlWrapper(contactConrollers.patchContactController));
+
+contactsRouter.delete("/:id", ctrlWrapper(contactConrollers.deleteContactController));
 
 export default contactsRouter;
