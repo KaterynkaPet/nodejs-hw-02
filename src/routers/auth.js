52ca7402d34a2f5e567de6034c9546c5c2f0b10a
KaterynkaPet <katerinapetrenko85@gmail.com>
2024-10-02 22:34:23 +0300

    add step 3

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,7 +5,9 @@ import * as authControllers from "../controllers/auth.js";
 import ctrlWrapper from "../utils/ctrlWrapper.js";
 import validateBody from "../utils/validateBody.js";
 
-import { userRegisterSchema, userLoginSchema } from "../validation/users.js";
+import { userRegisterSchema, userLoginSchema, requestResetEmailSchema } from "../validation/users.js";
+
+import { requestResetEmailController } from "../controllers/auth.js";
 
 const authRouter = Router();
 
@@ -16,5 +18,7 @@ authRouter.post("/login", validateBody(userLoginSchema), ctrlWrapper(authControl
 authRouter.post("/refresh", ctrlWrapper(authControllers.refreshController));
 
 authRouter.post("/logout", ctrlWrapper(authControllers.logoutController));
+
+authRouter.post("/send-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
 
 export default authRouter;
